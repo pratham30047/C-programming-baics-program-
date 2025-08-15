636ff510c215be3bb854c527fc1ccfbda9214d3e
pratham30047 <mandaliya.pratham8765@gmail.com>
2025-08-15 15:51:59 +0530

    Add files via upload

---
+++ all.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+
+{
+    int a,b,c,d,e;
+    float f;
+    printf("enter any first number ");
+    scanf("%d",&a);
+    printf("enter  second number");
+    scanf("%d",&b);
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a%b;
+    printf("the sum of entered two numbers is %d\n",c);
+     printf("the sub of entered two numbers is %d\n",d);
+      printf("the product of entered two numbers is %d\n",e);
+       printf("the division of entered two numbers is %f\n",f);
+
+
+
+
+
+}
---
+++ rupeestodollar.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{     /* dollar thi rupees*/
+    float a,b;
+    printf("enter any figure in rupees\n");
+    scanf("%f",&a);
+    b=a/48;
+    printf("%f",b);
+
+
+
+}
+
